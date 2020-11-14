88dcb5c048f71d392a551f25314d92da8a320ad1
Wilfried Mercier <wilfried.mercier58@gmail.com>
2020-11-14 13:32:12 +0100

    Added the possibility to save the configuration and reset it

--- TopWatch.py
+++ TopWatch.py
@@ -8,6 +8,9 @@ from   PyQt5.QtWidgets import QApplication, QAction, QLabel, QSizePolicy, QMainW
 from   PyQt5.QtGui     import QFont, QColor, QIcon
 from   PyQt5.QtCore    import Qt, QPoint, QTimer, QDateTime
 
+# Own imports
+import setup
+
 
 class App(QMainWindow):
     def __init__(self, *arg, **kwargs):
@@ -22,17 +25,22 @@ class App(QMainWindow):
         scriptDir = opath.dirname(opath.realpath(__file__))
         self.setWindowIcon(QIcon(opath.join(scriptDir, 'icon.png')))
 
+        # Setup initial color and font
+        configuration, ok = setup.init()
+        self.color = configuration['color']
+        self.font  = QFont()
+        self.font.fromString(configuration['font'])
+
         # Add label
         self.label = QLabel('', self)
         self.label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.label.setAlignment(Qt.AlignCenter)
-        self.font       = QFont('fixed', 30, QFont.Bold)
-        self.label.setStyleSheet('QLabel { color: #ffdd1c}')
+        self.label.setStyleSheet('QLabel { color: %s}' %self.color)
         self.label.setFont(self.font)
         self.showTime()
-        
+
         self.setCentralWidget(self.label)
-        
+
         # Set menu
         menubar        = self.menuBar()
         colorAction    = QAction('&Text color', self)
@@ -44,10 +52,24 @@ class App(QMainWindow):
         fontAction.setShortcut('Ctrl+F')
         fontAction.setStatusTip('Change text font')
         fontAction.triggered.connect(self.changeFont)
-        
+
+        resetAction    = QAction('&Reset', self)
+        resetAction.setShortcut('Ctrl+Alt+R')
+        resetAction.setStatusTip('Reset configuration')
+        resetAction.triggered.connect(self.reset)
+
+        saveAction     = QAction('&Save', self)
+        saveAction.setShortcut('Ctrl+S')
+        saveAction.setStatusTip('Save current configuration')
+        saveAction.triggered.connect(self.save)
+
+        self.filemenu    = menubar.addMenu('&File')
+        self.filemenu.addAction(saveAction)
+
         self.settingmenu = menubar.addMenu('&Settings')
         self.settingmenu.addAction(colorAction)
         self.settingmenu.addAction(fontAction)
+        self.settingmenu.addAction(resetAction)
 
         # Start timer
         self.timer     = QTimer()
@@ -69,9 +91,10 @@ class App(QMainWindow):
     def changeColor(self, *args, **kwargs):
         '''Ask for a text color and change it.'''
 
-        color = QColorDialog.getColor()
+        color         = QColorDialog.getColor()
         if color.isValid():
-           self.label.setStyleSheet('QLabel { color: %s }' %color.name())
+           self.color = color
+           self.label.setStyleSheet('QLabel { color: %s }' %self.color.name())
         return
 
     def changeFont(self, *args, **kwargs):
@@ -99,6 +122,19 @@ class App(QMainWindow):
         self.updateFont()
         return
 
+    def reset(self, *args, **kwargs):
+       '''Reset the configuration.'''
+
+       # Reset interface
+       self.color = '#ffdd1c'
+       self.font.fromString('fixed,30,-1,5,75,0,0,0,0,0')
+       self.label.setStyleSheet('QLabel { color: %s }' %self.color)
+       self.updateFont()
+
+       # Reset configuration file
+       setup.default('settings.yaml')
+       return
+
     def updateFont(self, *args, **kwargs):
         '''Update the label font with the value given in self.font and update the window size accordingly.'''
 
@@ -106,6 +142,15 @@ class App(QMainWindow):
         width  = self.label.fontMetrics().width(self.label.text())+2
         height = self.label.fontMetrics().height()+2
         self.setFixedSize(width, height)
+        return
+
+    def save(self, *args, **kwargs):
+        '''Save the current configuration.'''
+
+        configuration = {'font':self.font.toString(),
+                         'color':self.color.name()
+                        }
+        setup.writeConfiguration('settings.yaml', configuration)
         return
 
     def showTime(self, *args, **kwargs):

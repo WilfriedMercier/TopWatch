3d2d2d47f66c00e9d8d88dfabe83456d1f3c3b3f
Wilfried Mercier <wilfried.mercier58@gmail.com>
2021-04-17 12:41:36 +0200

    setup has been extended to take into account blinking and also checks that parameters have correct types and values

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ Setting up the program at startup.
 """
 
 import os
-import os.path as     opath
-from   yaml    import load, dump
-from   yaml    import Loader, Dumper
+import os.path      as     opath
+from   PyQt5.QtCore import QTime
+from   yaml         import load, dump, Loader, Dumper
 
 def default(outname, *args, **kwargs):
    '''
@@ -19,11 +19,16 @@ def default(outname, *args, **kwargs):
          name of the output YAML file
    '''
 
-   configuration = {'font'    : 'fixed,30,-1,5,75,0,0,0,0,0',
-                    'color'   : '#ffdd1c',
-                    'x'       : 0,
-                    'y'       : 0
+   configuration = {'font'        : 'fixed,30,-1,5,75,0,0,0,0,0',
+                    'color'       : '#ffdd1c',
+                    'x'           : 0,
+                    'y'           : 0,
+                    'opacity'     : 1,
+                    'blinkPeriod' : '00:00:01',
+                    'blinkFreq'   : 100,
+                    'blinkNb'     : 3
                    }
+
    writeConfiguration(outname, configuration)
    return
 
@@ -39,7 +44,7 @@ def writeConfiguration(outname, configuration, *args, **kwargs):
          name of the output YAML file
    '''
 
-   output         = dump(configuration, Dumper=Dumper)
+   output = dump(configuration, Dumper=Dumper)
    with open(outname, 'w') as f:
       f.write(output)
    return
@@ -60,20 +65,76 @@ def init(scriptDir, *args, **kwargs):
 
    # If the file does not exist, a default one is created
    if not opath.isfile(file):
-      default(file, 0, 0)
+      default(file)
 
    # Load configuration option from setting file
    with open(file, 'r') as f:
       settings    = load(f, Loader=Loader)
 
-   # If a key is missing, the setting file is saved and a new one is created with default values
+   # If a key is missing, the setting file is saved and a new one is created with default value
    errCode        = 0
-   for i in ['font', 'color', 'x', 'y']:
+   for i in ['font', 'color', 'x', 'y', 'opacity', 'blinkPeriod', 'blinkFreq', 'blinkNb']:
       if i not in settings.keys():
          print('Error in setting file %s. The key %s was missing. Generating a new default configuration file instead.' %(file, i))
-         os.rename(r'%s' %file, r'~%s' %file)
-         default(file, x, y)
+
+         # Save copy
+         path, newname = opath.split(file)
+         newname       = opath.join(path, r'~%s' %newname)
+         os.rename(file, newname)
+
+         # Generate and load new default settings
+         default(file)
          settings = load(file, Loader=Loader)
          errCode  = -1
+
+   ################################################
+   #               Check parameters               #
+   ################################################
+
+   # X coordinate
+   if not isinstance(settings['x'], int) or settings['x'] < 0:
+      print('Given x coordinate is < 0 or is not an int. Using 0 as default value instead.')
+      settings['x'] = 0
+   else:
+      settings['x'] = int(settings['x'])
+
+   # Y coordinate
+   if not isinstance(settings['y'], int) or settings['y'] < 0:
+      print('Given y coordinate is < 0 or is not an int. Using 0 as default value instead.')
+      settings['y'] = 0
+   else:
+      settings['y'] = int(settings['y'])
+
+   # Opacity
+   if not isinstance(settings['opacity'], (int, float)) or settings['opacity'] < 0 or settings['opacity'] > 1:
+      print('Given opacity is not in the range [0, 1] or is not an int/float. Using 1 as default value instead.')
+      settings['opacity']      = 1
+
+   # Period is changed from a string to a PyQt Qtime object
+   period = QTime().fromString(settings['blinkPeriod'])
+   if period.isNull():
+      print('Given blinking period could not be broadcasted to a valid PyQt QTime object. Using 1s as default value instead.')
+      settings['blinkPeriod']  = Qtime(0, 0, 1)
+   else:
+      settings['blinkPeriod']  = period
+
+   # Blinking frequency
+   if not isinstance(settings['blinkFreq'], (int, float)):
+      print('Given bliking frequency is not an int/float. Using 100ms as default value instead.')
+      settings['blinkFreq']    = 100
+   else:
+      if settings['blinkFreq'] < 50:
+         print('Given blinking frequency is below minimum value. Clipping to 50ms as default value instead.')
+         settings['blinkFreq'] = 50
+      elif settings['blinkFreq'] > 10000:
+         print('Given bliking frequency is above maximum value. Clipping to 10s as default value instead.')
+         settings['blinkFreq'] = 10000
+      else:
+         settings['blinkFreq'] = int(settings['blinkFreq'])
+
+   # Blinking number
+   if not isinstance(settings['blinkNb'], int) or settings['blinkNb'] <= 0:
+      print('Given blinking number is <= 0 or is not an int. Using 3 as default value instead.')
+      settings['blinkNb']      = 3
 
    return settings, errCode
